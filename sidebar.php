6656e88f9cf783dfa4d0a60e5e3ceea9d97081a7
wadatbara25 <wadatbara25@gmail.com>
2025-07-26 18:35:10 +0300

    sidebarEdit

--- sidebar.php
+++ sidebar.php
@@ -12,15 +12,15 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <aside class="sidebar" role="navigation" aria-label="القائمة الجانبية">
     <div class="logo" aria-label="شعار النظام">
-        <img src="http://localhost/RMS&Certificate/img/AdminLTELogo.png" alt="شعار نظام الشهادات" />
+        <img src="../img/AdminLTELogo.png" alt="شعار نظام الشهادات" />
     </div>
     <h2>نظام بيانات الطلاب</h2>
     <ul>
-        <li><a href="http://localhost/RMS&Certificate/student.php">طباعة الشهادات</a></li>
-        <li><a href="http://localhost/RMS&Certificate/university_payment_api/admin_payments.php">الرسوم الدراسية</a></li>
+        <li><a href="../student.php">طباعة الشهادات</a></li>
+        <li><a href="../university_payment_api/admin_payments.php">الرسوم الدراسية</a></li>
         <li><a href="#">التقارير</a></li>
-        <li><a href="http://localhost/RMS&Certificate/dashboard.php" class="<?= basename($_SERVER['PHP_SELF']) === 'dashboard.php' ? 'active' : '' ?>">لوحة التحكم</a></li>
-        <li><a href="http://localhost/RMS&Certificate/logout.php">تسجيل الخروج</a></li>
+        <li><a href="../dashboard.php" class="<?= basename($_SERVER['PHP_SELF']) === 'dashboard.php' ? 'active' : '' ?>">لوحة التحكم</a></li>
+        <li><a href="../logout.php">تسجيل الخروج</a></li>
     </ul>
 </aside>
 </head>
